c23e75ad919f4f064098032da08a8a59f29e78cb
Evgeniy <deyneko55@gmail.com>
2018-04-22 10:03:14 +0300

    fix

--- slavikbv/src/main/java/Lesson3.java
+++ slavikbv/src/main/java/Lesson3.java
@@ -2,7 +2,9 @@
  * Created by Student on 4/22/2018.
  */
 public class Lesson3 {
-public static void main (String[] args){}
-int a=3; int b=3;
-System.out.println("Hello world");
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+        System.out.println("Hello world");
+    }
 }
