36212ac50b5df6b186e67dab44c68ffdc17be2bb
vo4uk3 <vo4uk3@gmail.com>
2018-04-22 09:33:08 +0300

    first svovk commit and push

--- svovk/src/main/java/FirstCommitTest.java
+++ svovk/src/main/java/FirstCommitTest.java
@@ -1,4 +1,4 @@
-public class firstCommitTest {
+public class FirstCommitTest {
     public static void main(String[] args) {
         System.out.println("Success");
         System.out.println("Success");
