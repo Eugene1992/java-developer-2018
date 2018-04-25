59716bbf96a74520fd3a1c26414d714eee4310d4
vo4uk3 <vo4uk3@gmail.com>
2018-04-25 22:42:46 +0300

    Homework1

--- svovk/src/main/java/conditionalsLesson/conditionalsLessonTask2.java
+++ svovk/src/main/java/conditionalsLesson/conditionalsLessonTask2.java
@@ -8,16 +8,18 @@ import java.util.Scanner;
 
 public class conditionalsLessonTask2 {
     public static void main(String[] args) {
+
+        double c = 10;
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter first number");
-
-        int a = scan.nextInt();
+         double a = scan.nextInt();
 
         System.out.println("Enter second number");
 
-        int b = scan.nextInt();
-           if((a > b)) {
+        double b = scan.nextInt();
+           if(a - (c) > b - (c))   {
             System.out.println("closer to 10");
         } else {
                 System.out.println("not closer");
