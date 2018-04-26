8b72b92e2034e6c955283090f8b2e03c0589f5de
Василий <mr_mer@ukr.net>
2018-04-26 11:48:10 +0300

    ++

---
+++ vasya/src/main/java/Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите два числа: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println("Выберите арефметическое действие:");
+        String calc = scan.next();
+        switch (calc) {
+            case "+":
+                System.out.println((a + b));
+                break;
+            case "-":
+                System.out.println((a - b));
+                break;
+            case "*":
+                System.out.println((a * b));
+                break;
+            case "/":
+                if (b == 0)
+                    System.out.println("Нельзя делить на ноль");
+                else
+                    System.out.println((a / b));
+                break;
+            default:
+                System.out.println("Уведомление об ошибке");
+        }
+    }
+}
