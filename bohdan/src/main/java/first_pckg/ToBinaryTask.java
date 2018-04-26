81621db07442997fa871bfe50ebe9fd5242f9566
admin <admin@192.168.0.32>
2018-04-26 23:54:25 +0300

    Loops Homework added

--- bohdan/src/main/java/first_pckg/ToBinaryTask.java
+++ bohdan/src/main/java/first_pckg/ToBinaryTask.java
@@ -5,17 +5,16 @@ public class ToBinaryTask {
 	public static void main(String[] args) {
 		int x = 20;
 		String s = "";
-		
+
 		while (x > 0) {
-			s = (x%2) + s;
+			s = (x % 2) + s;
 			x /= 2;
 		}
 		System.out.println(s);
 
-		
-		for (;x > 0;x /= 2) {
-			s = (x%2) + s;
-		}		
+		for (; x > 0; x /= 2) {
+			s = (x % 2) + s;
+		}
 		System.out.println(s);
 
 	}
