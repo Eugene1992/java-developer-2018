586c0274609e8568fe18415c45d9034b648caa71
admin <admin@DESKTOP-KCKKACV>
2018-04-26 20:28:45 +0300

    LoopsTasks added

---
+++ bohdan/src/main/java/first_pckg/LoopsTaska.java
@@ -0,0 +1,12 @@
+package first_pckg;
+
+public class LoopsTaska {
+	public static void main(String[] args) {
+		int x = 0;
+		for (int i = 1; i <= 100; i++) {
+			x += i;
+		}
+		System.out.println(x);
+	}
+
+}
