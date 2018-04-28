e3f240ced2a0345731743be60d9ebc2d40066706
admin <admin@192.168.0.32>
2018-04-28 22:31:02 +0300

    Palindromes added

--- bohdan/src/main/java/first_pckg/MethodTask.java
+++ bohdan/src/main/java/first_pckg/MethodTask.java
@@ -7,11 +7,32 @@ public class MethodTask {
 		for (int i = begin; i <= end; i++) {
 			sum += i;
 		}
-		System.out.println(sum);
+		System.out.println(sum + "\n");
+	}
+
+	public static void printPalindromes() {
+		int result = 0, count = 0;
+		for (int i = 11; i < 10000; i++) {
+			int x = i;
+			result = 0;
+			while (x != 0) {
+				result *= 10;
+				result += x % 10;
+				x /= 10;
+			}
+			if (result == i) {
+				count++;
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println("\nAmount: " + count);
 	}
 
 	public static void main(String[] args) {
+
 		printIntervalSum(4, 10);
+
+		printPalindromes();
 
 	}
 
