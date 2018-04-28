aa3ca3e5c3b2c4ae1084b2996a7f0dc09081080a
Sasha <38600011+llKidoll@users.noreply.github.com>
2018-04-28 10:32:11 +0300

    Delete Home3.java

--- kido/src/main/java/Home3.java
+++
@@ -1,9 +0,0 @@
-public class Home3 {
-
-    //3. Инициализировать переменную типа short  результатом вычитания двух любых чисел
-
-    public static void main(String[] args) {
-        short e1 = 30021 - 14965;
-        System.out.println(e1);
-    }
-}
