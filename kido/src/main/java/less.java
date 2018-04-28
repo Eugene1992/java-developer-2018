54f645eb6be996f7d9a98304b422e9d941ab49b0
Sasha <38600011+llKidoll@users.noreply.github.com>
2018-04-28 10:15:58 +0300

    2First commit

--- kido/src/main/java/less.java
+++ kido/src/main/java/less.java
@@ -1,7 +1,7 @@
 /**
  * Created by Student on 4/22/2018.
  */
-public class less {
+public class Less {
     public static void main (String[] args){
         System.out.println ("Hello World!!");
     }
