ca145d7f9fc177f2a6bda32fabc813f3affbd651
Evgeniy <deyneko55@gmail.com>
2018-04-28 21:09:01 +0300

    add loops examples

--- java-starter/src/main/java/loops/Test.java
+++ java-starter/src/main/java/loops/Test.java
@@ -1,14 +1,6 @@
 package loops;
 
 public class Test {
-    public static void main(String[] args) {
-//  10100
-        int a = 41;
-        String res = "";
-        while (a > 0) {
-            res = (a % 2) + res;
-            a = a / 2;
-        }
-        System.out.println(res);
+    public static void main(String[] args) throws InterruptedException {
     }
 }
