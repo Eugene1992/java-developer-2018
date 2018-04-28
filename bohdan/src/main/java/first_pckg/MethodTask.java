97f4c03c3fa5b7cef04b13d448ef80549adce760
admin <admin@DESKTOP-KCKKACV>
2018-04-28 20:28:04 +0300

    upd

--- bohdan/src/main/java/first_pckg/MethodTask.java
+++ bohdan/src/main/java/first_pckg/MethodTask.java
@@ -1,15 +1,15 @@
 package first_pckg;
 
 public class MethodTask {
-	
-	public static void printIntervalSum (int begin, int end) {
+
+	public static void printIntervalSum(int begin, int end) {
 		int sum = 0;
 		for (int i = begin; i <= end; i++) {
 			sum += i;
 		}
 		System.out.println(sum);
 	}
-	
+
 	public static void main(String[] args) {
 		printIntervalSum(4, 10);
 
