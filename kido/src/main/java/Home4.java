25c59c3c0837f9a77b4d94427b2318cce75e5344
Sasha <38600011+llKidoll@users.noreply.github.com>
2018-04-28 10:32:05 +0300

    Delete Home4.java

--- kido/src/main/java/Home4.java
+++
@@ -1,9 +0,0 @@
-public class Home4 {
-
-    //4. Инициализировать переменную типа int результатом деления двух любых чисел
-
-    public static void main(String[] args) {
-        int w1 = 16 / 3;
-        System.out.println(w1);
-    }
-}
