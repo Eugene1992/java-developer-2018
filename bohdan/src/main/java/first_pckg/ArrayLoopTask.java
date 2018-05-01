38613c42eddb61fb67d37f6b66b5c561c21c932c
DanTe067 <teslyabya@gmail.com>
2018-05-01 20:53:32 +0300

    ArrayLoopTask updated

--- bohdan/src/main/java/first_pckg/ArrayLoopTask.java
+++ bohdan/src/main/java/first_pckg/ArrayLoopTask.java
@@ -31,15 +31,39 @@ public class ArrayLoopTask {
         return arr;
     }
 
+    static void getHalfWithMin(int[] arr) {
+        int min = arr[0];
+        int buf = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                buf = i;
+            }
+        }
+        if (buf < (arr.length / 2)) {
+            System.out.print("Min: " + min + "\nHalf with min: ");
+            for (int i = 0; i < (arr.length / 2); i++) {
+                System.out.print(arr[i] + " ");
+            }
+        } else {
+            System.out.print("Min: " + min + "\nHalf with min: ");
+            for (int i = (arr.length / 2); i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] arr = new int[5];
+        int[] arr = new int[6];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(10);
         }
         System.out.println(Arrays.toString(arr));
         System.out.println("Sum: " + getArraySum(arr));
         System.out.println("Max: " + getArrayMax(arr));
-        System.out.println("Method array:\n" + Arrays.toString(getFilledArray(5, -10, 20)));
+        System.out.println("Method array:\n" + Arrays.toString(getFilledArray(6, -10, 20)));
+        System.out.println("\n" + Arrays.toString(arr));
+        getHalfWithMin(arr);
     }
 }
