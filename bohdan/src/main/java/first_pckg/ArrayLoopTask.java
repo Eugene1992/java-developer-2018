e68cb2a991c7e01ad076de0c16499606b2ebfdf0
DanTe067 <teslyabya@gmail.com>
2018-05-01 20:29:18 +0300

    ArrayLoopTask added

---
+++ bohdan/src/main/java/first_pckg/ArrayLoopTask.java
@@ -0,0 +1,35 @@
+package first_pckg;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayLoopTask {
+    static int getArraySum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    static int getArrayMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(10);
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Sum: " + getArraySum(arr));
+        System.out.println("Max: " + getArrayMax(arr));
+    }
+}
