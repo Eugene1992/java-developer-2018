7da0d8b0afac94dcc7e48a8c9b1b7fab4a868d49
VisualOption <Gunmetal90@gmail.com>
2018-05-03 15:21:49 +0300

    HW Loops 1-5

---
+++ ruslan/src/main/java/loops/LoopsD.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class LoopsD {
+    public static void main(String[] args) {
+        for (int e = 2, r = 0 ; r != 20 ; e*=2 , r++) {
+            System.out.println(e);
+        }
+    }
+}
+
+//4. Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….
