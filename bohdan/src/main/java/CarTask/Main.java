869fe8f592b5c06e8f8e3a4cdc7b6452502e3021
DanTe067 <teslyabya@gmail.com>
2018-05-05 21:00:24 +0300

    CarTask added

---
+++ bohdan/src/main/java/CarTask/Main.java
@@ -0,0 +1,18 @@
+package CarTask;
+
+public class Main {
+    public static void main(String[] args) {
+        Car[] cars = {new Car("Lada", 80),
+                new Car("BMW", 180),
+                new Car("Lamborghini", 320)};
+
+        getDistance(cars, 3);
+    }
+
+    static void getDistance(Car[] cars, int time) {
+        for (Car i : cars) {
+            System.out.println(i.name + " will ride " + i.speed * time + " km for " + time + " hour(s)");
+        }
+    }
+
+}
