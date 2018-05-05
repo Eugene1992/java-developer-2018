a0b13581b975414c075479314bd64b543e20c889
Sasha <llKidoll@bigmir.net>
2018-05-05 21:06:47 +0300

    Car class

---
+++ slavikbv/src/main/java/ArraysHomeWorkNew/Car.java
@@ -0,0 +1,14 @@
+package ArraysHomeWorkNew;
+
+/**
+ * Created by Student on 5/5/2018.
+ */
+public class Car {
+    String name;
+    int speed;
+
+    Car (String name, int speed){
+        this.name = name;
+        this.speed = speed;
+    }
+}
