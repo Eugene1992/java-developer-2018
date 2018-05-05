941d90c79750fc733f17a8ea2126f10985814aef
PhurBaa <curam89@gmail.com>
2018-05-05 16:45:04 +0300

    Check point lessons

--- vitaliy/src/main/java/hwLessonLoops05/Task05.java
+++ vitaliy/src/main/java/hwLessonLoops05/Task05.java
@@ -4,7 +4,7 @@ public class Task05 {
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
-        for(int i=1;i <20;i++){
+        for (int i = 1; i < 20; i++) {
             int summ = a + b;
             a = b;
             b = summ;
