cc9fe2218a7b8300e129ab99d44865b7e75a85dd
DanTe067 <teslyabya@gmail.com>
2018-05-09 22:04:03 +0300

    tradeOOPHomework + encapsulation

--- bohdan/src/main/java/tradeOOPHomework/Category.java
+++ bohdan/src/main/java/tradeOOPHomework/Category.java
@@ -7,7 +7,16 @@ public class Category {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String toString() {
         return this.name;
     }
+
 }
