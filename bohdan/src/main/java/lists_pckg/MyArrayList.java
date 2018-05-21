9bb08b1c85866ec509e651ddc60ba950bb0fca06
DanTe067 <teslyabya@gmail.com>
2018-05-21 14:39:23 +0300

    list_pckg updated

---
+++ bohdan/src/main/java/lists_pckg/MyArrayList.java
@@ -0,0 +1,102 @@
+package lists_pckg;
+
+import java.util.Arrays;
+
+public class MyArrayList {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    Object[] list;
+    private int size;
+    private String listClass;
+
+    public MyArrayList() {
+        this.list = new Object[10];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity >= 0) {
+            this.list = new Object[initialCapacity];
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " +
+                    initialCapacity);
+        }
+    }
+
+//    public MyArrayList(Object[] objects){}
+
+    public void add(Object element) {
+        if (this.list[0] != null) {
+            if (this.listClass != element.getClass().toString()) {
+                throw new IllegalArgumentException("Illegal element type: " + element.getClass() + ", needed " + listClass);
+            }
+        }
+
+        if (size == (list.length - 1)) {
+            list = Arrays.copyOf(list, (list.length - 1) * (3 / 2) + 1);
+        }
+        list[++size] = element;
+    }
+
+    public void add(int index, Object element) {
+        if (this.list[0] != null) {
+            if (this.listClass != element.getClass().toString()) {
+                throw new IllegalArgumentException("Illegal element type: " + element.getClass() + ", needed " + listClass);
+            }
+        }
+        if (index < 0 || index > size + 1) {
+            throw new ArrayIndexOutOfBoundsException("Illegal index: " + index);
+        }
+
+        if (index == size) {
+            if (size == (list.length - 1)) {
+                list = Arrays.copyOf(list, (list.length - 1) * (3 / 2) + 1);
+            }
+            list[++size] = element;
+        } else if (index == 0) {
+            if (size == (list.length - 1)) {
+                list = Arrays.copyOf(list, (list.length - 1) * (3 / 2) + 1);
+            }
+            System.arraycopy(list, 0, list, 1, list.length - 1);
+            list[0] = element;
+            ++size;
+        } else {
+            if (size == (list.length - 1)) {
+                list = Arrays.copyOf(list, (list.length - 1) * (3 / 2) + 1);
+            }
+            System.arraycopy(list, index, list, index + 1, list.length - 1);
+            list[index] = element;
+            ++size;
+        }
+    }
+
+    public void set(int index, Object element) {
+        if (this.list[0] != null) {
+            if (this.listClass != element.getClass().toString()) {
+                throw new IllegalArgumentException("Illegal element type: " + element.getClass() + ", needed " + listClass);
+            }
+        }
+        if (index < 0 || index > size + 1) {
+            throw new ArrayIndexOutOfBoundsException("Illegal index: " + index);
+        }
+
+        list[index] = element;
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index > size + 1) {
+            throw new ArrayIndexOutOfBoundsException("Illegal index: " + index);
+        }
+
+        return list[index];
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index > size + 1) {
+            throw new ArrayIndexOutOfBoundsException("Illegal index: " + index);
+        }
+
+        --size;
+
+    }
+
+}
