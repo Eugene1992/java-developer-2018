2350ae64b597e9670ad44a678d9ae8783cd7f468
Evgeniy <deyneko55@gmail.com>
2018-06-12 10:35:19 +0300

    add examples

---
+++ java-professional/src/main/java/hashmap/Product.java
@@ -0,0 +1,42 @@
+package hashmap;
+
+public class Product {
+    private long num;
+    private String name;
+    private int price;
+
+    public Product(long num, String name, int price) {
+        this.num = num;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (num != product.num) return false;
+        if (price != product.price) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (num ^ (num >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + price;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "num=" + num +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
