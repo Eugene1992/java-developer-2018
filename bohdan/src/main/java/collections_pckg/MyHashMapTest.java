78ea6189e6bfb3af0741f7c60511e973d39780ad
DanTe067 <teslyabya@gmail.com>
2018-06-15 15:40:20 +0300

    MyHashMap updated, remove method TODO remaining

--- bohdan/src/main/java/collections_pckg/MyHashMapTest.java
+++ bohdan/src/main/java/collections_pckg/MyHashMapTest.java
@@ -11,9 +11,17 @@ public class MyHashMapTest {
         map.put(2125, "test2");
         map.put(0, "test0");
         map.put(1092, "test10");
+        map.put(6578, "test4");
+        map.put(324, "test6");
+        map.put(89, "test7");
+        map.put(46, "test9");
+        map.put(293, "test11");
+        map.put(5, "test5");
+        map.put(764, "test8");
+        map.put(37, "test99");
         map.printMap();
 
-        map.put(4, "test4");
+        map.put(324, "test4");
         map.put(3, "test33");
         map.printMap();
 
@@ -21,7 +29,7 @@ public class MyHashMapTest {
         map.printMap();
 
         System.out.println(map.contains(3));
-        System.out.println(map.contains(5));
+        System.out.println(map.contains(7));
 
         System.out.println(map.getValue(3));
         System.out.println(map.getValue(0));
