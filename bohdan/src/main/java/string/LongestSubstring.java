905dab9dd6e946e38539e2032c953b10f71a4ac9
DanTe067 <teslyabya@gmail.com>
2018-06-19 20:05:26 +0300

    StringTask updated

--- bohdan/src/main/java/string/LongestSubstring.java
+++ bohdan/src/main/java/string/LongestSubstring.java
@@ -5,10 +5,11 @@ public class LongestSubstring {
         String first = "Hello world!";
         String second = "Worse hell";
 
-        System.out.println(getLongestSubstring(first.toCharArray(), second));
+        System.out.println(getLongestSubstring(first, second));
     }
 
-    static String getLongestSubstring(char[] first, String second) {
+    static String getLongestSubstring(String firstStr, String second) {
+        char[] first = firstStr.toCharArray();
         String longestSubstring = "";
         for (int i = 0; i < first.length - 1; i++) {
             for (int j = i + 1; j < first.length; j++) {
