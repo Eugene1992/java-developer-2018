24d28520278e38ab3dcf37e02c83dd2b79e16372
Evgeniy <deyneko55@gmail.com>
2018-07-31 21:00:52 +0300

    add examples

--- java-fx/src/main/java/Controller.java
+++ java-fx/src/main/java/Controller.java
@@ -1,14 +1,11 @@
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.util.Callback;
 
 public class Controller {
 
