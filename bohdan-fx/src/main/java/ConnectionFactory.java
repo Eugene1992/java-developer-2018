9e8e3eadafedeaf22a6c09663b38508e056354da
DanTe067 <teslyabya@gmail.com>
2018-08-03 23:17:36 +0300

    latest added

---
+++ bohdan-fx/src/main/java/ConnectionFactory.java
@@ -0,0 +1,48 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+
+    public static final String CREDS_PROPERTIES = "postgresql_creds.properties";
+    private static final String
+            DRIVER = "driver",
+            URL = "url",
+            USERNAME = "username",
+            PASSWORD = "password";
+    private static Properties credsProperties;
+    private static Connection connection;
+
+    static {
+        try {
+            credsProperties = new Properties();
+            credsProperties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(CREDS_PROPERTIES));
+
+            Class.forName(credsProperties.getProperty(DRIVER));
+            connection = DriverManager.getConnection(
+                    credsProperties.getProperty(URL),
+                    credsProperties.getProperty(USERNAME),
+                    credsProperties.getProperty(PASSWORD)
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ConnectionFactory() {
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
---
+++ bohdan-fx/src/main/java/EmpDBApplication.java
@@ -0,0 +1,95 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EmpDBApplication extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    private EmployeeDaoImpl crud = new EmployeeDaoImpl();
+
+
+    //    Данные, в виде наблюдаемого списка адресатов.
+    private ObservableList<Employee> personData = FXCollections.observableArrayList();
+
+    public EmpDBApplication() {
+        // В качестве образца добавляем некоторые данные
+        personData.addAll(crud.getAll());
+    }
+
+    /**
+     * Возвращает данные в виде наблюдаемого списка адресатов.
+     *
+     * @return
+     */
+    public ObservableList<Employee> getPersonData() {
+        return personData;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Инициализирует корневой макет.
+     */
+    public void initRootLayout() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(EmpDBApplication.class.getResource("RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Показывает в корневом макете сведения об адресатах.
+     */
+    public void showPersonOverview() {
+        try {
+            // Загружаем сведения об адресатах.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(EmpDBApplication.class.getResource("EmployeeOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Помещаем сведения об адресатах в центр корневого макета.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Возвращает главную сцену.
+     *
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
